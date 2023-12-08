b3f0865948e03731fcb43bc643ff8401ea6915c6
Farook <mohammedfarook085@gmail.com>
2023-12-09 01:30:30 +0530

    header is a complete

--- src/components/headers/heading.jsx
+++ src/components/headers/heading.jsx
@@ -1,21 +1,22 @@
-import mountain from "../../assets/images/bg-header-desktop.png";
 import logo from "../../assets/images/logo.svg";
 const Heading = () => {
   return (
     <div className="text-center font-bai ">
-      <img src={mountain} alt="" />
-      <img className=" m-auto mt-[-9rem]" src={logo} alt="" />
-      <p className="text-[40px]  font-[600] mt-14">
+      <img className=" m-auto mt-[10.5rem]" src={logo} alt="" />
+      <p className="text-[40px] text-[#4C545D]  font-[600] mt-14">
         A history of everything you copy
       </p>
-      <p className="text-[18px] font-[400] mt-3">
-        {" "}
+      <p className="text-[18px] text-[#9FABB2] font-[400] mt-3">
         Clipboard allows you to track and organize everything you copy.
         Instantly <br /> access your clipboard on all your devices.
       </p>
-      <div className="flex gap-8  w-[20rem] m-auto">
-        <div className="bg-slate-400 px-20 h-12">Download for IOS</div>
-        <div className="bg-slate-300 h-12">Download for MAc</div>
+      <div className="flex w-[30rem] mt-10 m-auto font-[600]">
+        <div className="bg-[#26BAA4] text-white rounded-full m-auto py-3 h-12 w-[14rem]">
+          Download for IOS
+        </div>
+        <div className="bg-[#6173FF] text-white rounded-full m-auto py-3 h-[3rem] w-[14rem]">
+          Download for Mac
+        </div>
       </div>
     </div>
   );
