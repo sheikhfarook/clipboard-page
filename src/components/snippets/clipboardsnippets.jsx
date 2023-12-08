c9b2e822c66ea7b20875ada425b47367513071d2
Farook <mohammedfarook085@gmail.com>
2023-12-09 01:58:30 +0530

    complete the snippets

--- src/components/snippets/clipboardsnippets.jsx
+++ src/components/snippets/clipboardsnippets.jsx
@@ -1,3 +1,4 @@
+import monitor from "../../assets/images/image-computer.png";
 const ClipboardSnippets = () => {
   return (
     <div className="font-bai mt-36 ">
@@ -10,6 +11,42 @@ const ClipboardSnippets = () => {
           can access <br /> your snippets immediately on all your devices. Our
           Mac and iOS apps will help you <br /> organize everything.
         </p>
+      </div>
+      {/* img */}
+      <div className="flex mt-32 gap-[8rem] ">
+        <div>
+          <img className="" src={monitor} alt="" />
+        </div>
+        <div className="mt-16  leading-8">
+          <div>
+            <h3 className="font-[600] text-[#4C545D] text-[25px]">
+              Quick Search
+            </h3>
+            <p className="text-[18px] text-[#9FABB2] font-[400]">
+              Easily search your snippets by content, <br /> category, web
+              address, application, and more.
+            </p>
+          </div>
+          <div className="mt-16">
+            <h3 className="font-[600] text-[#4C545D] text-[25px]">
+              iCloud Sync
+            </h3>
+            <p className="text-[18px] text-[#9FABB2] font-[400]">
+              Instantly saves and syncs snippets across all <br />
+              your devices.
+            </p>
+          </div>
+          <div className="mt-16">
+            <h3 className="font-[600] text-[#4C545D] text-[25px]">
+              {" "}
+              Complete History
+            </h3>
+            <p className="text-[18px] text-[#9FABB2] font-[400]">
+              Retrieve any snippets from the first moment <br /> you started
+              using the app.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
